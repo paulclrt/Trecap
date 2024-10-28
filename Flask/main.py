8969c33e671b4e71cf85b596932e7a4c3703c253
pauclrt <p4ul.claret@gmail.com>
2024-10-27 19:57:05 -0500

    v1 flask done

--- Flask/main.py
+++ Flask/main.py
@@ -1,13 +1,21 @@
 from flask import Flask
-from flask import session, request, url_for, render_template
+from flask import session, request, url_for, render_template, redirect
 
 app = Flask("Flask recap app")
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/' # user for session ana cryptography of flask
+# here is how to generate a good secret key
+# $ python -c 'import secrets; print(secrets.token_hex())'
+# '192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf'
+
+@app.errorhandler(404) #redirect from every 404 in code - they can be executed by abort(404)...
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
 
 
 @app.route("/", methods=["GET", "POST"]) # can also do @app.get or @app.post
 def root():
     if request.method == "GET":
-        return "Hello World", 200
+        return redirect(url_for("query_param", q="redirect_from_home"))
     else:
         return render_template("tempaltes/home.html", person="jean")
 
@@ -40,7 +48,10 @@ def data():
     cookie = request.cookies.get("cookie_name")
     resp = make_response(render_template("home.html", person="cookie example"))
     resp.set_cookie("chocolat_chips", "best", max_age=60, ) # max age is seconds
-
+    resp.headers["X_Something"] = "A value"
+    session["user_id"] = 1
+    session.clear()
+    return resp
 
 
 
---
+++ Flask/users.py
@@ -0,0 +1,12 @@
+
+
+""" This file is not a real user implementation or anyting but routing + others functionalities """
+
+
+# To flash a message use the flash() method, to get hold of the messages you can use get_flashed_messages() which is also available in the templates.
+
+app.logger.debug('A value for debugging')
+app.logger.warning('A warning occurred (%d apples)', 42)
+app.logger.error('An error occurred')
+
+
